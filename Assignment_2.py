870ff291e23ba0c7860df9faef6e005e9db2b446
AgentR04 <136494277+AgentR04@users.noreply.github.com>
2023-06-27 18:45:54 +0530

    Add files via upload
    Assignment 2-Rutu Mehta

---
+++ Assignment_2.py
@@ -0,0 +1,70 @@
+# arithmetic operators
+a=24
+b=13
+print("addition: ",a+b)
+print("subtraction: ",a-b)
+print("multiplication: ",a*b)
+print("division: ",a/b)
+print("floor division: ",a//b)
+print("exponent: ",a**b)
+print("mod: ",a%b)
+
+# assignment operators
+a=24
+a+=1
+print("+=",a)
+a=13
+a-=1
+print("-=",a)
+a=24
+a*=1
+print("*=",a)
+a=13
+a/=1
+print("/=",a)
+a=24
+a**=1
+print("**=",a)
+a=13
+a//=1
+print("//=",a)
+a=24
+a%=1
+print("%=",a)
+
+# comparision operators / Relational operators
+a=24
+b=13
+print(">=",a>=b)
+print("<=",a<=b)
+print(">",a>b)
+print("<",a<b)
+print("==",a==b)
+print("!=",a!=b)
+
+#logical operators
+a=True
+b=False
+print("and",a and b)
+print("or",a or b)
+print("not",not b)
+
+#bitwise operators
+a=24
+b=13
+print("bitwise &",a&b)
+print("bitwise |",a|b)
+print("bitwise ^",a^b)
+print("bitwise >>",a>>b)
+print("bitwise <<",a<<b)
+
+#identity operators
+a=24
+b=13
+print("is",a is b)
+print("is not",a is not b)
+
+#membership operators
+a="rutu"
+print('u' in a)
+print("a" not in a)
